de64104654e0e46acdca9da3031e7dcb69b1ffec
bernie43 <hello@bernhardcaspar.de>
2025-10-31 16:03:41 +0000

    Improve devcontainer

--- .devcontainer/dev.md
+++ .devcontainer/dev.md
@@ -5,4 +5,5 @@ https://docs.openwebui.com/getting-started/advanced-topics/development
 npm run dev               # Frontend
 cd backend && sh dev.sh   # Backend
 
-
+Data dir: backend/data
+User: admin@admin.com - admin
--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -4,5 +4,7 @@ sudo chown -R vscode:vscode node_modules
 npm install --force
 npm run build
 
+sudo chown -R vscode:vscode /var/cache/pip
+pip config set global.cache-dir /var/cache/pip
 cd backend && \
    pip install -r requirements.txt -U
