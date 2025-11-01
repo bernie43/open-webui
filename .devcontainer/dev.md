5192309dba984cd4f12e4d72ccdac847f2c22219
bernie43 <hello@bernhardcaspar.de>
2025-11-01 04:41:30 +0000

    Improve dev server

--- .devcontainer/dev.md
+++ .devcontainer/dev.md
@@ -2,8 +2,8 @@
 
 https://docs.openwebui.com/getting-started/advanced-topics/development
 
-npm run dev               # Frontend
-cd backend && sh dev.sh   # Backend
+npm run dev          # Frontend
+backend/dev.sh       # Backend
 
 Data dir: backend/data
 User: admin@admin.com - admin
--- backend/dev.sh
+++ backend/dev.sh
@@ -1,3 +1,6 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR" || exit
+
 export CORS_ALLOW_ORIGIN="http://localhost:5173;http://localhost:8080"
 PORT="${PORT:-8080}"
 uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
