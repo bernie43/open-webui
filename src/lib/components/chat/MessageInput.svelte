654b810ede1ba3288ce8f7f614cd792b339aea50
bernie43 <hello@bernhardcaspar.de>
2025-12-03 19:38:11 +0700

    Add input blur timeout buffer

--- src/lib/components/chat/MessageInput.svelte
+++ src/lib/components/chat/MessageInput.svelte
@@ -144,7 +144,7 @@
 	const handleSubmit = () => {
 		dispatch('submit', prompt);
 		if ($mobile) {
-			setTimeout(() => document.getElementById('chat-input')?.blur(), 0);
+			setTimeout(() => document.getElementById('chat-input')?.blur(), 100);
 		}
 	};
 
